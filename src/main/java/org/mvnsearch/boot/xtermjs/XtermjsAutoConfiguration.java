73c877b5bd970373f77dbb895c2c885f6c216b15
linux_china <libing.chen@gmail.com>
2020-01-17 17:46:18 -0800

    Xterm.js auto configuration

---
+++ src/main/java/org/mvnsearch/boot/xtermjs/XtermjsAutoConfiguration.java
@@ -0,0 +1,30 @@
+package org.mvnsearch.boot.xtermjs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.ParameterResolver;
+import org.springframework.shell.standard.commands.Help;
+
+import java.util.List;
+
+/**
+ * Xterm.js auto configuration
+ *
+ * @author linux_china
+ */
+@Configuration
+public class XtermjsAutoConfiguration {
+    private Logger log = LoggerFactory.getLogger(XtermjsAutoConfiguration.class);
+
+    @Bean
+    public Help help(List<ParameterResolver> parameterResolvers) {
+        return new Help(parameterResolvers);
+    }
+
+    @Bean
+    public XtermRSocketController xtermRSocketController() {
+        return new XtermRSocketController();
+    }
+}

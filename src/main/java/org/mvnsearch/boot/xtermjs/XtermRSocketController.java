4eca0a13f3f2f4e4be8059d69ad7aa293fcba00b
linux_china <libing.chen@gmail.com>
2020-01-17 17:49:02 -0800

    xterm rsocket controller

---
+++ src/main/java/org/mvnsearch/boot/xtermjs/XtermRSocketController.java
@@ -0,0 +1,53 @@
+package org.mvnsearch.boot.xtermjs;
+
+import org.intellij.lang.annotations.Language;
+import org.intellij.lang.annotations.Subst;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import reactor.core.publisher.Flux;
+
+/**
+ * xterm rsocket controller
+ *
+ * @author linux_china
+ */
+@Controller
+public class XtermRSocketController extends XtermCommandHandler {
+    @Autowired
+    private Environment environment;
+
+    @MessageMapping("xterm.shell")
+    public Flux<String> shell(Flux<String> commands) {
+        return commands
+                .filter(data -> !data.trim().isEmpty())
+                .map(this::executeCommand);
+    }
+
+    @GetMapping(value = "/xterm", produces = "text/html; charset=utf-8")
+    @ResponseBody()
+    public String xterm() {
+        @Subst("/rsocket")
+        String path = environment.getProperty("spring.rsocket.server.mapping-path");
+        @Language("HTML")
+        String html = "<!doctype html>\n" +
+                "<html lang=\"en\">\n" +
+                "<head>\n" +
+                "    <meta charset=\"UTF-8\">\n" +
+                "    <title>Xterm</title>\n" +
+                "    <style>\n" +
+                "        html, body {\n" +
+                "            height: 100%;\n" +
+                "        }\n" +
+                "    </style>\n" +
+                "</head>\n" +
+                "<body>\n" +
+                "<xterm-console rsocket=\"" + path + "\"></xterm-console>\n" +
+                "<script type=\"text/javascript\" src=\"/xterm.bundle.js\"></script></body>\n" +
+                "</html>";
+        return html;
+    }
+}

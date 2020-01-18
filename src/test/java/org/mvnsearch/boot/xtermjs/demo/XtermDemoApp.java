7ab9e983bc82db65ce84524a605dced1709922ac
linux_china <libing.chen@gmail.com>
2020-01-17 17:55:52 -0800

    xterm js demo app

---
+++ src/test/java/org/mvnsearch/boot/xtermjs/demo/XtermDemoApp.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.boot.xtermjs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * xterm js demo app
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class XtermDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(XtermDemoApp.class, args);
+    }
+}

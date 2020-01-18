901426d067352d2e5aa352473f674c35a380e119
linux_china <libing.chen@gmail.com>
2020-01-17 18:00:14 -0800

    webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: './src/main/frontend/console.js',
+    output: {
+        path: path.resolve(__dirname, 'target/classes/META-INF/resources'),
+        filename: 'xterm.bundle.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.wasm', '.mjs']
+    }
+};

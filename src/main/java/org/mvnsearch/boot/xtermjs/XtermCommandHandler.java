1c4aaa6888f8da98d5e1485c9ff872f0414f5e14
linux_china <libing.chen@gmail.com>
2020-01-17 17:46:03 -0800

    Xterm command handler

---
+++ src/main/java/org/mvnsearch/boot/xtermjs/XtermCommandHandler.java
@@ -0,0 +1,34 @@
+package org.mvnsearch.boot.xtermjs;
+
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.shell.Shell;
+
+/**
+ * Xterm command handler: execute the commands from xterm.js
+ *
+ * @author linux_china
+ */
+public class XtermCommandHandler {
+    @Autowired
+    private Shell shell;
+
+    public String executeCommand(String commandLine) {
+        Object result = this.shell.evaluate(() -> commandLine);
+        String textOutput;
+        if (result instanceof Exception) {
+            textOutput = new AttributedString(result.toString(), AttributedStyle.DEFAULT.foreground(AttributedStyle.RED)).toAnsi();
+        } else if (result instanceof AttributedString) {
+            textOutput = ((AttributedString) result).toAnsi();
+        } else {
+            textOutput = result.toString();
+        }
+        //text format for Xterm
+        if (textOutput.contains("\n") && !textOutput.contains("\r\n")) {
+            return textOutput.replaceAll("\n", "\r\n");
+        } else {
+            return textOutput;
+        }
+    }
+}

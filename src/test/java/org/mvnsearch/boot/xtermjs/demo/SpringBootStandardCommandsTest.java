4ddff17566c3ed845c43c1a905ffab4df757f571
linux_china <libing.chen@gmail.com>
2020-01-24 13:03:45 -0800

    remove spel command test

--- src/test/java/org/mvnsearch/boot/xtermjs/demo/SpringBootStandardCommandsTest.java
+++ src/test/java/org/mvnsearch/boot/xtermjs/demo/SpringBootStandardCommandsTest.java
@@ -48,12 +48,6 @@ public class SpringBootStandardCommandsTest {
 	}
 
 	@Test
-	public void testSpelCommand() {
-		Object result = shell.evaluate(() -> "spel @userService.findById(1)");
-		System.out.println(result);
-	}
-
-	@Test
 	public void testBeanCommand() {
 		Object result = shell.evaluate(() -> "bean userService");
 		System.out.println(result);

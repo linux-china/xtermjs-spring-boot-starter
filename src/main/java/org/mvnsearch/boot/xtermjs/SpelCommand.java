76b34e9035a2aa432bb9f6faff6707584a26aafd
linux_china <libing.chen@gmail.com>
2020-01-24 13:02:30 -0800

    spel command to execute SpEL expression

---
+++ src/main/java/org/mvnsearch/boot/xtermjs/SpelCommand.java
@@ -0,0 +1,58 @@
+package org.mvnsearch.boot.xtermjs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanExpressionContext;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.expression.BeanExpressionContextAccessor;
+import org.springframework.context.expression.BeanFactoryResolver;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * spel command to execute SpEL expression
+ *
+ * @author linux_china
+ */
+public class SpelCommand implements CustomizedCommand {
+
+	@Autowired
+	private ConfigurableListableBeanFactory beanFactory;
+
+	final ExpressionParser spelParser = new SpelExpressionParser();
+
+	private StandardEvaluationContext spelContext;
+
+	private BeanExpressionContext rootObject;
+
+	@PostConstruct
+	public void init() {
+		this.spelContext = new StandardEvaluationContext();
+		this.spelContext.setVariable("systemProperties", System.getenv());
+		this.spelContext.setBeanResolver(new BeanFactoryResolver(this.beanFactory));
+		this.spelContext.addPropertyAccessor(new BeanExpressionContextAccessor());
+		this.rootObject = new BeanExpressionContext(beanFactory, null);
+	}
+
+	@Override
+	public String getName() {
+		return "spel";
+	}
+
+	@Override
+	public Object execute(String expressionText) {
+		Expression expression;
+		if (expressionText.startsWith("#{")) {
+			expression = spelParser.parseExpression(expressionText, new TemplateParserContext());
+		}
+		else {
+			expression = spelParser.parseExpression(expressionText);
+		}
+        return expression.getValue(spelContext, rootObject);
+	}
+
+}

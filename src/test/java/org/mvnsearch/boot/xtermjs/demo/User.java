f1d62984d1dcb2eb6b231ebd679e48e5c80e2b00
linux_china <libing.chen@gmail.com>
2020-01-24 09:55:59 -0800

    code format

--- src/test/java/org/mvnsearch/boot/xtermjs/demo/User.java
+++ src/test/java/org/mvnsearch/boot/xtermjs/demo/User.java
@@ -8,40 +8,44 @@ import java.util.Date;
  * @author linux_china
  */
 public class User {
-    private Integer id;
-    private String name;
-    private Date birth;
 
-    public User() {
-    }
+	private Integer id;
 
-    public User(Integer id, String name, Date birth) {
-        this.id = id;
-        this.name = name;
-        this.birth = birth;
-    }
+	private String name;
 
-    public Integer getId() {
-        return id;
-    }
+	private Date birth;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public User() {
+	}
 
-    public String getName() {
-        return name;
-    }
+	public User(Integer id, String name, Date birth) {
+		this.id = id;
+		this.name = name;
+		this.birth = birth;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public Date getBirth() {
-        return birth;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setBirth(Date birth) {
-        this.birth = birth;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
 }

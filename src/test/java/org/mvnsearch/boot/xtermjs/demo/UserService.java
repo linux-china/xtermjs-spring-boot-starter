93c6fcf0b34cac972dc3be9c5a0fa78248084804
linux_china <libing.chen@gmail.com>
2020-01-23 19:52:10 -0800

    user service

---
+++ src/test/java/org/mvnsearch/boot/xtermjs/demo/UserService.java
@@ -0,0 +1,17 @@
+package org.mvnsearch.boot.xtermjs.demo;
+
+import java.util.List;
+
+/**
+ * user service
+ *
+ * @author linux_china
+ */
+public interface UserService {
+
+	public String findRealName(Integer id);
+
+	public List<String> findVips(List<Integer> ids);
+
+    public void save(String name, Integer age) ;
+}

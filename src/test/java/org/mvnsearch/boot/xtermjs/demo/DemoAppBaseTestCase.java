966587fadf9cdd6091cf93448ce174793b23a35e
linux_china <libing.chen@gmail.com>
2020-01-26 14:23:06 -0800

    demo app base testcase

---
+++ src/test/java/org/mvnsearch/boot/xtermjs/demo/DemoAppBaseTestCase.java
@@ -0,0 +1,13 @@
+package org.mvnsearch.boot.xtermjs.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * demo app base testcase
+ *
+ * @author linux_china
+ */
+@SpringBootTest(classes = XtermDemoApp.class)
+public abstract class DemoAppBaseTestCase {
+
+}
